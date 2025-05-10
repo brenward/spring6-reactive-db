c20f2ab16a31db33b2d23bc9aa821b1e98159b4a
brenward <bren.ward@gmail.com>
2025-05-10 19:35:12 +0100

    #Adding date config

--- src/test/java/com/bwardweb/spring6_reactive_db/repositories/BeerRepositoryTest.java
+++ src/test/java/com/bwardweb/spring6_reactive_db/repositories/BeerRepositoryTest.java
@@ -1,15 +1,18 @@
 package com.bwardweb.spring6_reactive_db.repositories;
 
+import com.bwardweb.spring6_reactive_db.config.DatabaseConfig;
 import com.bwardweb.spring6_reactive_db.domain.Beer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.context.annotation.Import;
 
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataR2dbcTest
+@Import(DatabaseConfig.class)
 class BeerRepositoryTest {
 
     @Autowired
