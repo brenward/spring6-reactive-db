be0f0d52632108c4fd43855c01fabe8544cfd34e
brenward <bren.ward@gmail.com>
2025-05-10 19:13:38 +0100

    first commit

---
+++ src/main/java/com/bwardweb/spring6_reactive_db/Spring6ReactiveDbApplication.java
@@ -0,0 +1,13 @@
+package com.bwardweb.spring6_reactive_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6ReactiveDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6ReactiveDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bwardweb/spring6_reactive_db/Spring6ReactiveDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bwardweb.spring6_reactive_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6ReactiveDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
