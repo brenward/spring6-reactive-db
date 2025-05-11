5be9b4dede316d0c958c9e2aed4abeadde2f4c84
brenward <bren.ward@gmail.com>
2025-05-11 17:35:06 +0100

    #Using the webtestclient

---
+++ src/test/java/com/bwardweb/spring6_reactive_db/controllers/BeerControllerTest.java
@@ -0,0 +1,28 @@
+package com.bwardweb.spring6_reactive_db.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+class BeerControllerTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    void testListBeers(){
+        webTestClient.get()
+                .uri(BeerController.BEER_PATH)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().valueEquals("Content-Type", "application/json")
+                .expectBody().jsonPath("$.size()").isEqualTo(3);
+    }
+
+}
