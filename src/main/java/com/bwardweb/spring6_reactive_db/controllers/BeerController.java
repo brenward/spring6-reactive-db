28aa4654baca4d5cff01d9b654b564efeaa63416
brenward <bren.ward@gmail.com>
2025-05-11 10:55:02 +0100

    #adding find by id

--- src/main/java/com/bwardweb/spring6_reactive_db/controllers/BeerController.java
+++ src/main/java/com/bwardweb/spring6_reactive_db/controllers/BeerController.java
@@ -5,19 +5,27 @@ import com.bwardweb.spring6_reactive_db.model.BeerDTO;
 import com.bwardweb.spring6_reactive_db.services.BeerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequiredArgsConstructor
 public class BeerController {
 
     public static final String BEER_PATH = "/api/v2/beer";
+    public  static final String BEER_PATH_ID = "/api/v2/beer/{beerId}";
 
     private final BeerService beerService;
 
     @GetMapping(BEER_PATH)
     Flux<BeerDTO> listBeers() {
         return beerService.listBeers();
+    }
+
+    @GetMapping(BEER_PATH_ID)
+    Mono<BeerDTO> getBeerById(@PathVariable Integer beerId) {
+        return beerService.getBeerById(beerId);
     }
 }
--- src/main/java/com/bwardweb/spring6_reactive_db/services/BeerService.java
+++ src/main/java/com/bwardweb/spring6_reactive_db/services/BeerService.java
@@ -2,7 +2,10 @@ package com.bwardweb.spring6_reactive_db.services;
 
 import com.bwardweb.spring6_reactive_db.model.BeerDTO;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface BeerService {
     Flux<BeerDTO> listBeers();
+
+    Mono<BeerDTO> getBeerById(Integer beerId);
 }
