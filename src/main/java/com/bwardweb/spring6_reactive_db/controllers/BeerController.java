3efcfc4b44bd573b544ad029b7d49cfaca4c9228
brenward <bren.ward@gmail.com>
2025-05-11 10:31:30 +0100

    #Setting up reactive controller

---
+++ src/main/java/com/bwardweb/spring6_reactive_db/controllers/BeerController.java
@@ -0,0 +1,20 @@
+package com.bwardweb.spring6_reactive_db.controllers;
+
+
+import com.bwardweb.spring6_reactive_db.model.BeerDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+public class BeerController {
+
+    public static final String BEER_PATH = "/api/v2/beer";
+
+    @GetMapping(BEER_PATH)
+    Flux<BeerDTO> listBeers() {
+        return Flux.just(
+                BeerDTO.builder().id(1).build(),
+                BeerDTO.builder().id(2).build());
+    }
+}
